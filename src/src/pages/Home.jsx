5b4ebbff523562a9b72732b500a113e315aab7fc
shyynux <shyynux@gmail.com>
2023-10-09 21:03:24 +0530

    added tailwind css

---
+++ src/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='p-4 font-mono font-bold'> i-did-it
+        <div className='p-4'></div>
+    </div>
+  )
+}
+
+export default Home
